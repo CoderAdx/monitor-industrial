af43e9aff52cf3358aaf707d7d8bfcbda231cb68
CoderAdx <adryagiulyy@gmail.com>
2026-04-21 09:37:16 -0300

    study: scripts de estudo de variaves e arumentos bash

---
+++ scripts/argumentos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Script: $0"
+echo "Primeiro argumento: $1"
+echo "Segundo argumento: $2"
+echo "Terceiro argumentos: $3"
+
---
+++ scripts/variaveis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -u
+
+CODER="Adx"
+INTERVALO=${1:-5}
+echo "$CODER rodando os testes a cada $INTERVALO segundos"
